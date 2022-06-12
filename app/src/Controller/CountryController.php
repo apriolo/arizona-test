71e0ce1a1590bcbca92d71bf3aba3404dcefa841
Arian Priolo <arianpriolo@gmail.com>
2022-06-12 19:33:27 -0300

    Creating export Country CSV

--- app/src/Controller/CountryController.php
+++ app/src/Controller/CountryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Services\CountryCsvService;
 use App\Services\CountryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,10 +13,12 @@ use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 class CountryController extends AbstractController
 {
     private CountryService $countryService;
+    private CountryCsvService $countryCsvService;
 
-    public function __construct(CountryService $countryService)
+    public function __construct(CountryService $countryService, CountryCsvService $countryCsvService)
     {
         $this->countryService = $countryService;
+        $this->countryCsvService = $countryCsvService;
     }
 
     public function index(Request $request)
@@ -29,5 +32,21 @@ class CountryController extends AbstractController
                 'sorter' => $sorter
             ]
         );
+    }
+
+    public function exportCsv(Request $request)
+    {
+        $sorter = ($request->get('sort')) ?: 'name';
+        $file = $this->countryCsvService->exportToCsv($sorter);
+
+        $stream = new Stream($file['path']);
+        $response = new BinaryFileResponse($stream);
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->setContentDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            $file['name']
+        );
+
+        return $response;
     }
 }

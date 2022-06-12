dd8655dd9d6f2f0c22627bd7cb59ecad14984244
Arian Priolo <arianpriolo@gmail.com>
2022-06-12 19:14:48 -0300

    Creating country entity

---
+++ app/src/Entity/Country.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CountryRepository")
+ * @ORM\Table(name="Country")
+ */
+class Country
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $abbreviation;
+
+    /**
+     * @return mixed
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     * @return Country
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     * @return Country
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAbbreviation(): ?string
+    {
+        return $this->abbreviation;
+    }
+
+    /**
+     * @param mixed $abbreviation
+     * @return Country
+     */
+    public function setAbbreviation(string $abbreviation): self
+    {
+        $this->abbreviation = $abbreviation;
+        return $this;
+    }
+
+
+
+}
